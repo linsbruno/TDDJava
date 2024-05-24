426f3810128038a1ede6cb2c573889823de7eb01
Bruno Lins <linslimabruno@gmail.com>
2024-05-24 15:25:30 -0300

    primeiro commit

---
+++ test/linsbruno/tests/PrimeiroTest.java
@@ -0,0 +1,14 @@
+package linsbruno.tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PrimeiroTest {
+
+    @Test
+    public void teste(){
+
+    }
+}
